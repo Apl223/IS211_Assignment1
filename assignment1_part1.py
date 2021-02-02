3b3a55bda4029ee3d8f4af6f57c65a54b47895e2
Apl223 <36824547+Apl223@users.noreply.github.com>
2021-02-01 23:41:22 -0500

    Add files via upload

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,23 @@
-
-def listDivide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
-    pass
+def listDivide(numbers, divide=2):
+    counter = 0
+    for x in numbers:
+       if x%divide == 0:
+          counter += 1
+    print("Numbers divided evenly in the list: ", counter) 
+    return counter
 
 def testListDivide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+    try:
+      assert listDivide([1,2,3,4,5]) == 2
+      assert listDivide([2,4,6,8,10]) == 5
+      assert listDivide([30, 54, 63,98, 100], divide=10) == 2
+      assert listDivide([]) == 0
+      assert listDivide([1,2,3,4,5], 1) == 5
+    except:
+       raise ListDivideException()
+
+class ListDivideException(Exception):    
+      pass
+
 if __name__ == "__main__":
     testListDivide()
